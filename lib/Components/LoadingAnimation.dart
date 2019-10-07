84ba821ee1554a2afb0c74f9dcf5a85689fa2115
key kim <keyhyuk.kim@gmail.com>
2019-10-07 15:12:48 +0900

    Refactor animation and add pop scope in animation

--- lib/Components/LoadingAnimation.dart
+++ lib/Components/LoadingAnimation.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttertoast/fluttertoast.dart' as prefix0;
 
 class ModalAnimation extends ModalRoute<void> {
-
   Function _disposeFunction;
 
   ModalAnimation(void connectionClear()) {
     _disposeFunction = connectionClear;
   }
+
   @override
   Duration get transitionDuration => Duration(milliseconds: 500);
 
@@ -28,11 +30,9 @@ class ModalAnimation extends ModalRoute<void> {
   bool get maintainState => true;
 
   @override
-  Widget buildPage(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-  ) {
+  Widget buildPage(BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,) {
     // This makes sure that text and other content follows the material style
     return Material(
       type: MaterialType.transparency,
@@ -44,25 +44,56 @@ class ModalAnimation extends ModalRoute<void> {
   }
 
   Widget _buildOverlayContent(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            '넷플릭스 계정을 공유할 사람을 구하고있습니다.\n앱을 백그라운드로 하고 다른 작업을 하셔도 됩니다.',
-            style: TextStyle(color: Colors.white, fontSize: 15.0),
+    return WillPopScope(
+        onWillPop: () {
+          Fluttertoast.showToast(
+              msg: "파티 찾기를 취소했습니다.",
+              toastLength: Toast.LENGTH_LONG,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIos: 1,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0);
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                '앱을 백그라운드로 돌리고 다른 작업을 하셔도 됩니다.',
+                style: TextStyle(color: Colors.white, fontSize: 15.0),
+              ),
+              Text(
+                '파티를 찾으면 푸쉬알람을 보내드릴게요.',
+                style: TextStyle(color: Colors.white, fontSize: 15.0),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 40, bottom: 40),
+                child: SpinKitPouringHourglass(
+                  color: Colors.white,
+                  size: 50.0,
+                ),
+              ),
+              FlatButton(
+                color: Colors.red,
+                onPressed: () =>
+                {
+                  Fluttertoast.showToast(
+                      msg: "파티 찾기를 취소했습니다.",
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIos: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0),
+                  Navigator.pop(context),
+                },
+                child: Text('그만 찾기'),
+              )
+            ],
           ),
-          SpinKitRotatingPlain(
-          color: Colors.white,
-          size: 50.0,
-        ),
-          RaisedButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('Dismiss'),
-          )
-        ],
-      ),
-    );
+        ));
   }
 
   @override
@@ -80,8 +111,9 @@ class ModalAnimation extends ModalRoute<void> {
 
   @override
   void dispose() {
+    print('dispose');
+    Fluttertoast.cancel();
     _disposeFunction();
     super.dispose();
   }
 }
-
--- lib/Components/ProfileImageComponent.dart
+++ lib/Components/ProfileImageComponent.dart
@@ -4,7 +4,7 @@ class ProfileImageComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 60),
+      margin: EdgeInsets.only(top: 40, bottom: 60),
       child: Image.asset('lib/assets/images/netflix-chat-logo.png'),
     );
   }
